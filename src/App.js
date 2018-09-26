46804822d67aa7d508921c0198c9533d6038fc1c
Nikhil <nikhilbhatia2004@gmail.com>
2018-09-26 14:14:56 +0300

    second5

--- src/App.js
+++ src/App.js
@@ -11,16 +11,23 @@ class App extends React.Component {
       album:'',
       id:''
 
-    }}
+    },
+  playlistName:'',
+playlistTracks:{
+  name:'Random Name',
+  artist:'Random artist',
+  album:'Randm Album',
+  id:'Random iD'
+}}
   }
-  render() {
+    render() {
     return (<div>
   <h1>Ja<span className="highlight">mmm</span>ing</h1>
   <div className="App">
   <SearchBar />
     <div className="App-playlist">
       <SearchResults searchResults={this.state.searchResults} />
-      <Playlist />
+      <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
     </div>
   </div>
 </div>)
--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -4,6 +4,7 @@ class Playlist extends React.Component {
   render() {
     return (<div className="Playlist">
   <input defaultValue="New Playlist"/>
+  <TrackList tracks={this.props.playlistTracks} />
   <a class="Playlist-save">SAVE TO SPOTIFY</a>
 </div>)
   }
