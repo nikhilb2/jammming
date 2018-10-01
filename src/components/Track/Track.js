6d067034297435fa0de07b3394d5a51bd3198cca
Nikhil <nikhilbhatia2004@gmail.com>
2018-10-01 15:18:55 +0300

    asdsasdsdas

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -21,7 +21,7 @@ class Track extends React.Component {
     }
   }
   render() {
-    return (<div class="Track">
+    return (<div className="Track">
   <div className="Track-information">
     <h3>{this.props.track.name}</h3>
     <p>{this.props.track.artist} | {this.props.track.album}</p>
